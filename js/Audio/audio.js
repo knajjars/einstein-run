126490b1ff2137f87a735c4d4ad9194341ec5fdf
Khalil Najjar <knajjars@gmail.com>
2018-11-07 21:05:49 +0100

    github fix

--- js/Audio/audio.js
+++ js/Audio/audio.js
@@ -1,9 +1,15 @@
-var soundTrack = new Audio("../../../sounds/main-soundtrack.mp3");
+var soundTrack = new Audio(
+  "../../game-einstein-run/sounds/main-soundtrack.mp3"
+);
 soundTrack.loop = true;
 
-var rightAnswerSound = new Audio("../../../sounds/right-answer.wav");
-var wrongAnswerSound = new Audio("../../../sounds/wrong-answer.wav");
-var levelUpSound = new Audio("../../../sounds/level-up.wav");
+var rightAnswerSound = new Audio(
+  "../../game-einstein-run/sounds/right-answer.wav"
+);
+var wrongAnswerSound = new Audio("../../sounds/wrong-answer.wav");
+var levelUpSound = new Audio("../../sounds/level-up.wav");
 
-var scoreScreenSound = new Audio("../../../sounds/score-screen.mp3");
+var scoreScreenSound = new Audio(
+  "../../game-einstein-run/sounds/score-screen.mp3"
+);
 scoreScreenSound.loop = true;
