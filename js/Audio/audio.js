73091a02faff101318a23ed5c5c6e72d381ec0b6
Khalil Najjar <knajjars@gmail.com>
2018-11-07 21:07:43 +0100

    final bug fix

--- js/Audio/audio.js
+++ js/Audio/audio.js
@@ -6,8 +6,10 @@ soundTrack.loop = true;
 var rightAnswerSound = new Audio(
   "../../game-einstein-run/sounds/right-answer.wav"
 );
-var wrongAnswerSound = new Audio("../../sounds/wrong-answer.wav");
-var levelUpSound = new Audio("../../sounds/level-up.wav");
+var wrongAnswerSound = new Audio(
+  "../../game-einstein-run/sounds/wrong-answer.wav"
+);
+var levelUpSound = new Audio("../../game-einstein-run/sounds/level-up.wav");
 
 var scoreScreenSound = new Audio(
   "../../game-einstein-run/sounds/score-screen.mp3"
